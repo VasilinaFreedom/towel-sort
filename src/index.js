84a8fff56ea933119d57776a309a88a9b1158577
VasilinaFreedom <95901200+VasilinaFreedom@users.noreply.github.com>
2022-02-24 16:36:56 +0300

    Add solution towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,49 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(arguments.length===0){return []}
+    if(matrix.length===0){return []}
+      else if(matrix.length ===2){
+        let arr1 = []
+      arr1.unshift(matrix[0])
+      let a1= arr1.flat()
+      let arr2 = []
+      arr2.unshift(matrix[1])
+      let a2= arr2.flat()
+      a2.reverse()
+      let newArr = a1.concat(a2)
+      return newArr
+      }
+      else if(matrix.length===3){
+      let arr1 = []
+      arr1.unshift(matrix[0])
+      let a1= arr1.flat()
+      let arr2 = []
+      arr2.unshift(matrix[1])
+      let a2= arr2.flat()
+      a2.reverse()
+      let arr3 = []
+      arr3.unshift(matrix[2])
+      let a3= arr3.flat()
+      let newArr = a1.concat(a2,a3)
+      return newArr
+    }
+    else if(matrix.length===4){
+        let arr1 = []
+        arr1.unshift(matrix[0])
+        let a1= arr1.flat()
+        let arr2 = []
+        arr2.unshift(matrix[1])
+        let a2= arr2.flat()
+        a2.reverse()
+        let arr3 = []
+        arr3.unshift(matrix[2])
+        let a3= arr3.flat()
+        let arr4 = []
+        arr4.unshift(matrix[3])
+        let a4= arr4.flat()
+        a4.reverse()
+        let newArr = a1.concat(a2,a3, a4)
+        return newArr
+    }
 }
